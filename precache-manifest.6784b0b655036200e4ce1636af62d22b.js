c274e6481a3f5d55fb874889587830b7d7e289eb
RashedSharahili <RashedSharahili@users.noreply.github.com>
2023-09-09 02:14:34 +0000

    deploy: 591941bb97392a74f7d8e5c7c7bc37a4a6641d82

---
+++ precache-manifest.6784b0b655036200e4ce1636af62d22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a03778d56a34d20ffcc9",
+    "url": "/static/css/main.864bac69.chunk.css"
+  },
+  {
+    "revision": "a03778d56a34d20ffcc9",
+    "url": "/static/js/main.a03778d5.chunk.js"
+  },
+  {
+    "revision": "bb0ef1aa47cf3f4e5903",
+    "url": "/static/js/1.bb0ef1aa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d92db3e24340c626ac7714acc88b3938",
+    "url": "/static/media/logo-investech.d92db3e2.png"
+  },
+  {
+    "revision": "de72028ec2b8795a4098ab2662fb0f9e",
+    "url": "/index.html"
+  }
+];
