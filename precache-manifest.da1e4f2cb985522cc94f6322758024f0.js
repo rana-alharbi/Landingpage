17810b30970684a5518ddcd7f70a60c25001d9a2
RashedSharahili <rwishd17@gmail.com>
2023-09-09 06:39:19 +0300

    Updates

---
+++ precache-manifest.da1e4f2cb985522cc94f6322758024f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e0496f5058a9d70304",
+    "url": "/landingpage/static/css/main.1ede9aa5.chunk.css"
+  },
+  {
+    "revision": "50e0496f5058a9d70304",
+    "url": "/landingpage/static/js/main.50e0496f.chunk.js"
+  },
+  {
+    "revision": "c27c86df9131c355bdd4",
+    "url": "/landingpage/static/js/1.c27c86df.chunk.js"
+  },
+  {
+    "revision": "3bd09c43796294cb8952",
+    "url": "/landingpage/static/js/runtime~main.3bd09c43.js"
+  },
+  {
+    "revision": "42e276f83324427a5d50af4b942858b4",
+    "url": "/landingpage/index.html"
+  }
+];
